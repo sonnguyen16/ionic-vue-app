3502a8b7a46d98e272b686f81c68c6b62c0293b4
sonnguyen16 <sonnv2212@gmail.com>
2024-05-11 08:58:06 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'quan-ly-chung-cu-remake',
+  webDir: 'dist'
+};
+
+export default config;
